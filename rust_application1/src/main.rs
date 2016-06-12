9abb70a007babdf2620680d33da166274bb61402
isaev-bolsen <al_mamistov@mail.ru>
2016-06-12 10:50:22 +0300

    Compare

--- rust_application1/src/main.rs
+++ rust_application1/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::{i8,i32,f32};
+use std::cmp::Ordering;
 use rand::Rng;
 use std::io;
 
@@ -22,10 +23,21 @@ fn main()
     {
         println!("Guess the number!");
         let secret_number = rand::thread_rng().gen_range(1, 101);
+        
         println!("Please input your guess.");
         let mut guess = String::new();
+
         io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
         println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+                    Ordering::Less    => println!("Too small!"),
+                    Ordering::Greater => println!("Too big!"),
+                    Ordering::Equal   => println!("You win!"),
+            }
     }
 }
 
